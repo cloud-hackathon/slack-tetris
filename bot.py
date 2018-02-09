2d037b4f3a1ae924032859801bef752dafe4169d
htysh <h.ohta@ntt.com>
2018-02-09 18:20:59 +0900

    :recycle: modified for cloud foundry

--- bot.py
+++ bot.py
@@ -2,17 +2,19 @@ import logging
 import os
 import time
 from slackclient import SlackClient
+from slackeventsapi import SlackEventAdapter
 from tetris import Tetris, HEIGHT, WIDTH
 
 
-# get slack client
-sc = SlackClient(os.environ.get("SLACK_API_TOKEN"))
+# get slack client and event apapter
+sc = SlackClient(os.getenv("SLACK_API_TOKEN"))
+sea = SlackEventAdapter(os.getenv("SLACK_VERIFICATION_TOKEN"), "/events")
 
 # get tetris
 tetris = Tetris()
 
 # load bot info
-BOT_NAME = os.environ.get("BOT_NAME")
+BOT_NAME = os.getenv("BOT_NAME")
 bot_id = ""
 resp = sc.api_call("users.list")
 if resp.get('ok'):
@@ -163,61 +165,50 @@ def post_message(channel, command):
   )
 
 
-def handle_message(message):
-  command = message["text"].split()[1]
-  if command in omits.keys():
-    command = omits.get(command)
-  if command in ["left", "right"] and len(message["text"].split()) > 2:
-    try:
-      repeat = int(message["text"].split()[2])
-    except:
-      repeat = None
-  else:
-    repeat = None
-  channel = message["channel"]
-  if not tetris.player:
-    tetris.player = message["ts"]
-
-  try:
-    if command == "start":
-      if tetris.playing:
-        post_message(channel, "playing")
-      else:
-        eval(command)()
-        post_message(channel, command)
+@sea.on("message")
+def handle_message(event_data):
+  message = event_data.get("event")
+  mentions = [BOT_NAME, "<@%s>"%BOT_ID]
+  if message.get("text") and message["text"].split()[0] in mentions:
+    command = message["text"].split()[1]
+    if command in omits.keys():
+      command = omits.get(command)
+    if command in ["left", "right"] and len(message["text"].split()) > 2:
+      try:
+        repeat = int(message["text"].split()[2])
+      except:
+        repeat = None
     else:
-      if tetris.playing:
-        if repeat and repeat < WIDTH - 2:
-          for i in range(repeat):
-            eval(command)()
-          post_message(channel, command)
+      repeat = None
+    channel = message["channel"]
+    if not tetris.player:
+      tetris.player = message["ts"]
+
+    try:
+      if command == "start":
+        if tetris.playing:
+          post_message(channel, "playing")
         else:
-          if eval(command)():
+          eval(command)()
+          post_message(channel, command)
+      else:
+        if tetris.playing:
+          if repeat and repeat < WIDTH - 2:
+            for i in range(repeat):
+              eval(command)()
             post_message(channel, command)
           else:
-            post_message(channel, "cannot_move")
-      else:
-        post_message(channel, "not_playing")
-  except Exception as e:
-    logger.warn(e)
-    post_message(channel, texts.get("help"))
-
-
-def get_mentioned_message(message_list):
-  mentions = [BOT_NAME, "<@%s>"%BOT_ID]
-  if len(message_list) > 0:
-    for message in message_list:
-      if message.get("text") and message["text"].split()[0] in mentions:
-        return message
-  return {}
+            if eval(command)():
+              post_message(channel, command)
+            else:
+              post_message(channel, "cannot_move")
+        else:
+          post_message(channel, "not_playing")
+    except Exception as e:
+      logger.warn(e)
+      post_message(channel, texts.get("help"))
 
 
 if __name__ == "__main__":
-  # start to call real time messaging api
-  if sc.rtm_connect():
-    while True:
-      message = get_mentioned_message(sc.rtm_read())
-      if message:
-        handle_message(message)
-  else:
-    logger.warn("Connection Failed")
+  # start flask server
+  sea.start(host='0.0.0.0', port=int(os.getenv("PORT")), debug=True)
